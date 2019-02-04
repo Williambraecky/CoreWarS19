4e0d0b416025c95bf39ecb4dfd3167add7961ebe
William Braeckman <wbraeckm@student.s19.be>
2019-02-04 22:22:33 +0100

    Minor changes: - Optimize instruction and dlabel tokens - instruction token logic changes

--- srcs/asm/token/dlabel_token.c
+++ srcs/asm/token/dlabel_token.c
@@ -6,31 +6,17 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 14:48:20 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/02/04 21:50:18 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/02/04 22:00:45 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
-
-/*
-** TODO: Optimize of_type return lex error in make_token
-*/
 
 int		dlabel_of_type(char *line, size_t i)
 {
 	if (line[i] != DIRECT_CHAR || line[i + 1] != LABEL_CHAR)
 		return (0);
 	return (ft_strchr(LABEL_CHARS, line[i + 2]) != NULL);
-	// i += 2;
-	// while (line[i])
-	// {
-	// 	if (ft_strchr(SEPARATOR_CHARS, line[i]))
-	// 		break ;
-	// 	else if (!ft_strchr(LABEL_CHARS, line[i]))
-	// 		return (0);
-	// 	i++;
-	// }
-	// return (1);
 }
 
 t_token	dlabel_make_token(char *line, size_t i)
