8931d5b34e6d40b52ac4f5843b2c6e8ec15daf8a
Sebastien De Spiegeleer <sde-spie@student.42.fr>
2019-02-18 14:02:05 +0100

    edit usage

--- srcs/corewar/error.c
+++ srcs/corewar/error.c
@@ -6,7 +6,7 @@
 /*   By: sde-spie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 11:45:41 by sde-spie          #+#    #+#             */
-/*   Updated: 2019/02/04 15:52:39 by sde-spie         ###   ########.fr       */
+/*   Updated: 2019/02/18 14:01:32 by sde-spie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,15 @@ void		error_exit(t_vm *vm, char *msg)
 
 int			error_usage(void)
 {
-	ft_printf("Usage: ./corewar [-dump nbr_cycles] ");
-	ft_printf("[[-n number] champion1.cor] ...\n");
-	ft_printf("\t-dump nbr_cycles\t: ");
+	ft_printf("Usage: ./corewar [-dump nbr_cycles] [-visu] [-sounds] ");
+	ft_printf("[[-n number] champion1.cor] ...\n\n");
+	ft_printf("\t-dump nbr_cycles:\n\t\t\t\t");
 	ft_printf("Dumps memory after nbr_cycles then exits\n");
-	ft_printf("\t-n number\t\t: ");
+	ft_printf("\t-visu\t\t:\n\t\t\t\t");
+	ft_printf("Use the visual mode to display the battle, live.\n");
+	ft_printf("\t-sounds\t\t:\n\t\t\t\t");
+	ft_printf("Use sounds mode to add special effect to the battle.\n");
+	ft_printf("\t-n number\t:\n\t\t\t\t");
 	ft_printf("Set the number of the following champion to number.\n");
 	return (0);
 }
