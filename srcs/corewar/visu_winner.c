e325b154778cdd830dbd5d61edc5c90d56da2803
William Braeckman <wbraeckm@student.s19.be>
2019-02-28 14:57:39 +0100

    Center winner name

--- srcs/corewar/visu_winner.c
+++ srcs/corewar/visu_winner.c
@@ -6,7 +6,7 @@
 /*   By: sde-spie <sde-spie@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 17:05:59 by sde-spie          #+#    #+#             */
-/*   Updated: 2019/02/28 14:04:32 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/02/28 14:56:14 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,8 +70,9 @@ void		print_winner(t_vm *vm, WINDOW *window)
 		wprintw(window, "PLAYER %d WON!!", winner.number);
 		wattroff(window, COLOR_PAIR(6));
 		wattron(window, COLOR_PAIR(vm->arena.winner));
-		wmove(window, 46, 16);
-		wprintw(window, "%*s", 10, winner.code.header.prog_name);
+		wmove(window, 46, 26 -
+			ft_min(26, ft_strlen(winner.code.header.prog_name) / 2));
+		wprintw(window, "%s", winner.code.header.prog_name);
 		wattroff(window, COLOR_PAIR(vm->arena.winner));
 	}
 	else
