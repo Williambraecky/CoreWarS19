2d5d73c60cbd56ffa70cd0fd0bf28f9a9d621768
Charles Antoine Van Beers <charlesantoine.vanbeers@gmail.com>
2019-02-28 14:13:27 +0100

    Modified op_get_params.c so our VM mimics the behavior of the VM given regarding the coding octet

--- srcs/corewar/op_get_params.c
+++ srcs/corewar/op_get_params.c
@@ -6,11 +6,18 @@
 /*   By: sde-spie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 17:33:10 by sde-spie          #+#    #+#             */
-/*   Updated: 2019/02/25 10:54:22 by sde-spie         ###   ########.fr       */
+/*   Updated: 2019/02/28 14:06:55 by cvan-bee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+static int	sizearg(t_arg_type arg, int dir_size)
+{
+	if (arg != 2)
+		return ((arg % 2) ? arg : arg / 2);
+	return (dir_size);
+}
 
 static int	check_type_code(t_vm *vm, t_process *process, int i)
 {
@@ -21,26 +28,31 @@ static int	check_type_code(t_vm *vm, t_process *process, int i)
 	code = vm->arena.arena[(process->pc + 1) % MEM_SIZE] >> (6 - 2 * i) & 0x03;
 	code = (code == 3 ? 4 : code);
 	if (!(code & process->instruction.types[i]))
-		return (0);
+		ret = 0;//return (0);
 	process->instruction.types[i] = (char)code;
-	return (1);
+	process->instruction.adv += sizearg(process->instruction.types[i],
+			process->instruction.label_size ? 2: 4);
+	return (ret);
 }
 
 static int	check_code_octet(t_vm *vm, t_process *process)
 {
 	int		i;
+	int		ret;
 
+	ret = 1;
 	i = 0;
+	process->instruction.adv = 2;
 	while (i < process->instruction.nb_arg)
 	{
 		if (!check_type_code(vm, process, i))
 		{
 			process->instruction.types[0] = -1;
-			return (0);
+			ret = 0;//return (0);
 		}
 		i++;
 	}
-	while (i < 4)
+	/*while (i < 4)
 	{
 		if (vm->arena.arena[(process->pc + 1) % MEM_SIZE] >> (6 - 2 * i) & 0x03)
 		{
@@ -48,15 +60,8 @@ static int	check_code_octet(t_vm *vm, t_process *process)
 			return (0);
 		}
 		i++;
-	}
-	return (1);
-}
-
-static int	sizearg(t_arg_type arg, int dir_size)
-{
-	if (arg != 2)
-		return ((arg % 2) ? arg : arg / 2);
-	return (dir_size);
+	}*/
+	return (ret);
 }
 
 int			big_end_toi(unsigned char *arena, int pc, int size)
@@ -99,7 +104,7 @@ void		op_get_params(t_vm *vm, t_process *process)
 				process->instruction.value[i] > REG_NUMBER))
 		{
 			process->instruction.types[0] = -1;
-			return ;
+			//return ;
 		}
 	}
 	process->instruction.adv = adv;
